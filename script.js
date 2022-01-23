61d46bbba794c5db5f3770354e9edcd9b9ee54a6
SamuLucas <samulucasf@hotmail.com>
2022-01-23 15:27:38 -0300

    aprendendendo switch com exercícios

---
+++ script.js
@@ -0,0 +1,45 @@
+// Exercício 1
+
+let food = "lasanha";
+
+switch (food) {
+    case 'lasanha':
+        console.log("Não vendemos essa comida aqui!");
+        break;
+
+    case 'pizza':
+        console.log("Estamos com escassez de pizza!");
+        break;
+
+    case 'salada':
+        console.log('Aqui está, são 3 reais!');
+        break;
+
+    default:
+        console.log('Pedido Inválido!');
+}
+
+// Exercício 2
+
+let perfume = "florais";
+
+switch (perfume) {
+    case 'florais':
+        console.log('Compra efetuada com sucesso!');
+        break;
+
+    case 'amadeirados':
+        console.log('Tem certeza que não prefere outro aroma?');
+        break;
+
+    case 'cítricos':
+        console.log('Tem certeza que não prefere outro aroma?');
+        break;
+
+    case 'frutais':
+        console.log('Tem certeza que não prefere outro aroma?');
+        break;
+
+    default:
+        console.log('Não trabalhamos com esse tipo de aroma!');
+}
